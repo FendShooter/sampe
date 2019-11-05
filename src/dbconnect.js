986863f232c19ece82262a049405ceac399fc345
FendShooter <virgile.dokouvi@gmail.com>
2019-11-05 12:16:07 -0800

    best

--- src/dbconnect.js
+++ src/dbconnect.js
@@ -1,9 +1,9 @@
 const Pool = require("pg").Pool;
 const pool = new Pool({
   user: "postgres",
-  host: "localhost",
-  database: "dvdrental",
-  password: "1983",
+  host: "ec2-107-22-236-52.compute-1.amazonaws.com",
+  database: "d3b3jdjf18d17t",
+  password: "db593e450c888098740786f816bd25ea75d2aef57e9b4c7000f102422f5fb3d6",
   port: 5432
 });
 
